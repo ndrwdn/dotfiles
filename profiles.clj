4e6d2eda98169c8dd2dec67b902c677e0f330de8
Andrew Dean <ndrwdn@gmail.com>
2016-02-16 12:41:00 -0600

    Updating lein profile deps.

--- profiles.clj
+++ profiles.clj
@@ -2,15 +2,15 @@
  {:plugins
   [[speclj "3.3.1"]
    [lein-ring "0.9.7"]
-   [lein-ancient "0.6.7"]
-   [cider/cider-nrepl "0.9.1"]]
+   [lein-ancient "0.6.8"]
+   [cider/cider-nrepl "0.10.2"]]
   :dependencies [[spyscope "0.1.5"]
                  [im.chit/vinyasa "0.4.2"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [im.chit/iroh "0.1.11"]
-                 [io.aviso/pretty "0.1.19"]
+                 [io.aviso/pretty "0.1.23"]
                  [org.clojure/tools.nrepl "0.2.12"]
-                 [leiningen "2.5.3"]]
+                 [leiningen "2.6.1"]]
   :injections [(require 'spyscope.core)
                (require '[vinyasa.inject :as inject])
                (require 'io.aviso.repl)
