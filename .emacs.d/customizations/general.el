4411b8131201ff39a9f44a40e4a414c4130595d5
Andrew Dean <ndrwdn@gmail.com>
2017-09-06 13:06:25 -0500

    Adding paredit for elisp.

--- .emacs.d/customizations/general.el
+++ .emacs.d/customizations/general.el
@@ -52,3 +52,12 @@
 
 ;; Load adoc-mode
 (autoload 'adoc-mode "adoc-mode" nil t)
+
+;; Paredit for elisp
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
