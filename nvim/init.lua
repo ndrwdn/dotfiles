92a8db2f7c64ed7be039ce9c17b09b76f8cd86cf
Andrew Dean <ndrwdn@gmail.com>
2024-09-25 14:54:37 -0500

    feat(nvim): updates keybinding for completion confirmation

--- nvim/init.lua
+++ nvim/init.lua
@@ -495,7 +495,7 @@ require("lazy").setup({
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
             ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({
+            ["<Tab>"] = cmp.mapping.confirm({
               behavior = cmp.ConfirmBehavior.Insert,
               select = true
             })
