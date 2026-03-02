434c6fcdce761646d7e1018a2f6ce2f423d17d79
Andrew Dean <ndrwdn@gmail.com>
2026-03-02 08:14:25 -0600

    feat(nvim): updates to better rename prompt

--- nvim/init.lua
+++ nvim/init.lua
@@ -189,7 +189,30 @@ local function mappings(client, bufnr)
   local buf_project_opts = merge({ desc = "Search project-wide symbols" }, opts)
   local buf_ref = "<Cmd>lua vim.lsp.buf.references()<CR>"
   local buf_ref_opts = merge({ desc = "List all references" }, opts)
-  local buf_rename = "<Cmd>lua vim.lsp.buf.rename()<CR>"
+  local buf_rename = function()
+    -- when rename opens the prompt, this autocommand will trigger
+    -- it will "press" CTRL-F to enter the command-line window `:h cmdwin`
+    -- in this window I can use normal mode keybindings
+    local cmdId
+    cmdId = vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
+      callback = function()
+        local key = vim.api.nvim_replace_termcodes("<C-f>", true, false, true)
+        vim.api.nvim_feedkeys(key, "c", false)
+        vim.api.nvim_feedkeys("0", "n", false)
+        -- autocmd was triggered and so we can remove the ID and return true to delete the autocmd
+        cmdId = nil
+        return true
+      end,
+    })
+    vim.lsp.buf.rename()
+    -- if LPS couldn't trigger rename on the symbol, clear the autocmd
+    vim.defer_fn(function()
+      -- the cmdId is not nil only if the LSP failed to rename
+      if cmdId then
+        vim.api.nvim_del_autocmd(cmdId)
+      end
+    end, 500)
+  end
   local buf_rename_opts = merge({ desc = "Rename symbol" }, opts)
   local buf_sig_help = "<Cmd>lua vim.lsp.buf.signature_help()<CR>"
   local buf_sig_help_opts = merge({ desc = "Sig help (cursor over arg)" }, opts)
