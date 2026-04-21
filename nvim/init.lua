f90eb0eaee5b61ac12a1518fdce86b7bf20a5245
Andrew Dean <ndrwdn@gmail.com>
2026-04-20 20:47:07 -0500

    fix(nvim): keeps gitsigns and codediff from clashing

--- nvim/init.lua
+++ nvim/init.lua
@@ -807,29 +807,31 @@ require("lazy").setup({
           signcolumn = false,
           numhl = true,
           on_attach = function(bufnr)
-            local gitsigns = require('gitsigns')
-            local function map(mode, l, r, opts)
-              opts = opts or {}
-              opts.buffer = bufnr
-              vim.keymap.set(mode, l, r, opts)
+            if vim.g.loaded_codediff == nil then
+              local gitsigns = require('gitsigns')
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
+              map('n', ']c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({']c', bang = true})
+                else
+                  gitsigns.nav_hunk('next')
+                end
+              end)
+
+              map('n', '[c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({'[c', bang = true})
+                else
+                  gitsigns.nav_hunk('prev')
+                end
+              end)
+
+              map('n', 'z', "<Cmd>Gitsigns<CR>")
             end
-            map('n', ']c', function()
-              if vim.wo.diff then
-                vim.cmd.normal({']c', bang = true})
-              else
-                gitsigns.nav_hunk('next')
-              end
-            end)
-
-            map('n', '[c', function()
-              if vim.wo.diff then
-                vim.cmd.normal({'[c', bang = true})
-              else
-                gitsigns.nav_hunk('prev')
-              end
-            end)
-
-            map('n', 'z', "<Cmd>Gitsigns<CR>")
           end,
         })
       end
